4b6b41fb45cfaddee868212ba921aade3d851075
Athos-Sobreira-Monteiro <visokostore@gmail.com>
2024-10-11 21:39:45 -0300

    views.py

--- tarefa/views.py
+++ tarefa/views.py
@@ -2,36 +2,36 @@ from .models import Tarefa
 from .forms import TarefaForm
 from django.views.generic import CreateView, UpdateView, DeleteView, ListView
 
-# Listar tarefas
+
 class ListaTarefa(ListView):
     model = Tarefa
     template_name = 'lista.html'
     context_object_name = 'tarefas'
 
     def get_queryset(self):
-        queryset = super().get_queryset()  # Obtém o queryset padrão
-        search_query = self.request.GET.get('search', '')  # Captura o valor da pesquisa
+        queryset = super().get_queryset()  
+        search_query = self.request.GET.get('search', '')  
 
         if search_query:
-            queryset = queryset.filter(nome_tarefa__icontains=search_query)  # Filtra as tarefas
+            queryset = queryset.filter(nome_tarefa__icontains=search_query)  
 
-        return queryset  # Retorna o queryset filtrado
+        return queryset  
 
-# Nova Tarefa 
+
 class NovaTarefa(CreateView):
     model = Tarefa
     form_class = TarefaForm
     template_name = 'nova.html'
     success_url = '/lista'
 
-# Atualizar Tarefas
+
 class AtualizarTarefa(UpdateView):
     model = Tarefa
     form_class = TarefaForm
     template_name = 'atualizar.html'
     success_url = '/lista'
 
-# Apagar tarefas
+
 class DeletarTarefa(DeleteView):
     model = Tarefa
     template_name = 'deletar.html'
